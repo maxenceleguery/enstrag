9bf9ed2b5ae8e5d8c07a8c9364941f742974862a
maxenceleguery <maxence.leguery@gmail.com>
2024-12-16 09:25:15 +0100

    :sparkles: Adding possibility to reset the databse from command line

--- enstrag/__main__.py
+++ enstrag/__main__.py
@@ -3,11 +3,23 @@ from .rag import RagAgent
 from .models import get_pipeline, RagEmbedding
 from .data import VectorDB, Parser
 
+import argparse
+
+parser = argparse.ArgumentParser(prog='Enstrag')
+
+parser.add_argument('-r', '--reset', action='store_true', help="Reset the vector database on start.")
+args = parser.parse_args()
+
 llm_folder = "Qwen2.5-1.5B-Instruct"
 embedding_folder = "all-MiniLM-L6-v2"
 persist_directory="/tmp/enstrag"
 
 db = VectorDB(RagEmbedding(embedding_folder), persist_directory=persist_directory)
+
+if args.reset:
+    print("Resetting database...")
+    db.db.reset_collection()
+
 db.add_documents(
     Parser.get_documents_from_pdf_urls([
         "https://arxiv.org/pdf/1706.03762",
