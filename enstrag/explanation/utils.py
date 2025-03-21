bad243a936adc58bae044b92980f754f12b76957
Maxence Leguéry <105419587+maxenceleguery@users.noreply.github.com>
2025-03-21 10:35:23 +0100

    Explainable rag : gradient method (#14)
    * :sparkles: Define a new pertuber: Leave Nouns
    
    * :zap: propagate the pertubated token
    
    * Admin panel (#12)
    
    * :sparkles: Admin panel to add documents
    
    * :sparkles: Add doc by url or upload
    
    * :books: Create pull_request_template.md
    
    * :hammer: Gradio code refactor
    
    * :wrench: Fix tests
    
    * :clown_face: Fix tests
    
    * :clown_face: Fix tests again
    
    * :bookmark: Increment version
    
    * Separation of LLM and Gradio front (#13)
    
    * :sparkles: First tests with FastAPI
    
    * :sparkles: Separation of back and front
    
    * :bookmark: Incrementing version
    
    * :hammer: Fix CI
    
    * :sparkles: Explanation on gradio
    
    * :hammer: Fix docker compose
    
    * :hammer: Deactivate dev setup
    
    * :bookmark: Increment version
    
    * :sparkles: Implementation of gradient and refactor XRAGPipeline
    
    * :bug: Fix gradient based method
    
    * :hammer: Refactor tests
    
    * :hammer: Refactor tests 2
    
    * :wrench: Testing gradient with log likelihood
    
    * :sparkles: Skip special characters
    
    * :sparkles: add batch inference for pertubation method
    
    * :sparkles: Choice of explanation method
    
    ---------
    
    Co-authored-by: toniodo <antoinedo54@yahoo.fr>

--- enstrag/explanation/utils.py
+++ enstrag/explanation/utils.py
@@ -1,15 +1,14 @@
 """Utils function for the explainable part"""
 from numpy import dot, array
 from numpy.linalg import norm
-from typing import List
 
-def cosine_similarity(x: List[float], y: List[float]) -> float:
+def cosine_similarity(x: list[float], y: list[float]) -> float:
     """Compute the cosine similarity between two vectors"""
     a = array(x)
     b = array(y)
     return dot(a, b) / (norm(a) * norm(b))
 
-def euclidian_similarity(x: List[float], y: List[float]) -> float:
+def euclidian_similarity(x: list[float], y: list[float]) -> float:
     """Compute the cosine similarity between two vectors"""
     a = array(x)
     b = array(y)
